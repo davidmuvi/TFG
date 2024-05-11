a43ff21de3c2864d498900dd5124b2b7bef2a8a0
davidmuvi <davidmunozvillalba@gmail.com>
2024-05-12 00:41:48 +0200

    Employee CRUD

--- backend/controllers/employee.js
+++ backend/controllers/employee.js
@@ -10,6 +10,33 @@ export const createEmployee = async (req, res) => {
     const employeeSaved = await newEmployee.save()
     res.status(201).json(employeeSaved)
   } catch (error) {
-    res.status(500).json({ msg: error.message })
+    res.status(404).json({ message: 'Error creating employee' })
+  }
+}
+
+export const getAllEmployees = async (req, res) => {
+  try {
+    const employees = await Employee.find()
+    res.status(200).json(employees)
+  } catch (error) {
+    res.status(404).json({ message: 'Employees not found' })
+  }
+}
+
+export const getEmployeeById = async (req, res) => {
+  try {
+    const employee = await Employee.findById(req.params.id)
+    res.status(200).json(employee)
+  } catch (error) {
+    res.status(404).json({ message: 'Employee not found' })
+  }
+}
+
+export const updateEmployeeById = async (req, res) => {
+  try {
+    const employeeFound = await Employee.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.status(200).json(employeeFound)
+  } catch (error) {
+    res.status(404).json({ message: 'Employee not found' })
   }
 }
--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
-import { createEmployee } from '../controllers/employee.js'
+import { createEmployee, getAllEmployees, getEmployeeById, updateEmployeeById } from '../controllers/employee.js'
 import { validateSchema } from '../middlewares/schema_validator.js'
 import { createEmployeeSchema } from '../schemas/employee.js'
 
 const router = Router()
 
+router.get('/', getAllEmployees)
 router.post('/', validateSchema(createEmployeeSchema), createEmployee)
+router.get('/:id', getEmployeeById)
+router.patch('/:id', updateEmployeeById)
 
 export default router
