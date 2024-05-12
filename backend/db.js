b6d1e7756b5b073c14b63fc91fd179427f8a1b0a
davidmuvi <davidmunozvillalba@gmail.com>
2024-05-12 17:24:29 +0200

    gitignore

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose'
 import 'dotenv/config'
-
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://davidmunozvillalba:QF8ekq7BRxMyfawV@cluster0.xdeitbj.mongodb.net/restaurant?retryWrites=true&w=majority&appName=Cluster0')
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('MongoDB Connected')
   } catch (err) {
     console.error(err)
