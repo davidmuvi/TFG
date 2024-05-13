f1fed0ab03ac79177c2ffc52de305700e34dbbef
davidmuvi <davidmunozvillalba@gmail.com>
2024-05-13 14:05:52 +0200

    Employee Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
+import EmployeePage from './pages/EmployeePage';
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<LoginPage />} />
+      <Route path='/' element={<EmployeePage />} />
     </Routes>
      
     </BrowserRouter>
