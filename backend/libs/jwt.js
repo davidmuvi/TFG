d74f843e875416941077662364cb1510af1ace51
davidmuvi <davidmunozvillalba@gmail.com>
2024-05-24 12:26:28 +0200

    token expiration modified

--- backend/libs/jwt.js
+++ backend/libs/jwt.js
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
-export function createAccessToken (payload) {
+export function createAccessToken(payload) {
   return new Promise((resolve, reject) => {
     jwt.sign(
       payload,
       process.env.TOKEN_SECRET,
       {
-        algorithm: 'HS256', expiresIn: '1d'
+        algorithm: 'HS256', expiresIn: '6h'
       },
       (err, token) => {
         if (err) reject(err)
