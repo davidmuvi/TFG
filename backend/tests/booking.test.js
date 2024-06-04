672a03abf620e9bac4d4e8dde3b482503b968b47
davidmuvi <davidmunozvillalba@gmail.com>
2024-06-04 18:09:55 +0200

    some changes

--- backend/tests/booking.test.js
+++
@@ -1,9 +0,0 @@
-import app from '../app.js'
-import request from 'supertest'
-
-describe('GET /api/bookings', () => {
-    test('should response with a 200 status code', async () => {
-        const response = await request(app).get('/api/bookings').send()
-        console.log(response)
-    })
-})
