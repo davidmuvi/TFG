2ae08428d5f63ebe949582483a0e17f0c7e8dfbe
davidmuvi <davidmunozvillalba@gmail.com>
2024-06-10 18:17:37 +0200

    some changes

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -1,14 +1,14 @@
 import { Router } from 'express'
 import { employeeController } from '../controllers/employee.js'
 import { validateSchema } from '../middlewares/schema_validator.js'
-import { createEmployeeSchema } from '../schemas/employee.js'
+import { createEmployeeSchema, modifyEmployeeSchema } from '../schemas/employee.js'
 
 const router = Router()
 
 router.get('/', employeeController.getAllEmployees)
 router.post('/', validateSchema(createEmployeeSchema), employeeController.createEmployee)
 router.get('/:id', employeeController.getEmployeeById)
-router.patch('/:id', employeeController.updateEmployeeById)
+router.patch('/:id', validateSchema(modifyEmployeeSchema), employeeController.updateEmployeeById)
 router.delete('/:id', employeeController.deleteEmployeeById)
 
 export default router
--- backend/schemas/employee.js
+++ backend/schemas/employee.js
@@ -8,3 +8,11 @@ export const createEmployeeSchema = z.object({
   password: z.string({ required_error: 'Password is required' }).min(8, { message: 'Password must be at least 8 characters' }),
   role: z.string({ required_error: 'Role is required' })
 })
+
+export const modifyEmployeeSchema = z.object({
+  name: z.string({ required_error: 'Name is required' }),
+  telephone: z.string({ required_error: 'Telephone is required' }).regex(/^[6-7][0-9]{8}$/, 'Invalid phone number'),
+  email: z.string({ required_error: 'Email is required' }).email({ message: 'Invalid email' }),
+  username: z.string({ required_error: 'Username is required' }),
+  role: z.string({ required_error: 'Role is required' })
+})
